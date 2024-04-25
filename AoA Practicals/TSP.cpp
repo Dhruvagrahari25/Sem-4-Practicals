aca59ce7af08a514a60a01f9fa256466e1081680
Dhruv Harish Agrahari <122349013+Dhruvagrahari25@users.noreply.github.com>
2024-04-26 00:28:06 +0530

    Updated TSP

--- AoA Practicals/TSP.cpp
+++ AoA Practicals/TSP.cpp
@@ -1,44 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define INF 1e9
-
-int n;
-vector<vector<int>> dist;
-vector<vector<int>> dp;
-
-int tsp(int mask, int pos) {
-    if(mask == (1 << n) - 1)
-        return dist[pos][0]; // return to starting city
-
-    if(dp[mask][pos] != -1)
-        return dp[mask][pos];
-
-    int ans = INF;
-    for(int i = 0; i < n; i++) {
-        if((mask & (1 << i)) == 0) // city not yet visited
-            ans = min(ans, dist[pos][i] + tsp(mask | (1 << i), i));
-    }
-
-    return dp[mask][pos] = ans;
-}
-
-int main() {
-    cout << "Enter the number of cities: ";
-    cin >> n;
-
-    dist.assign(n, vector<int>(n));
-    dp.assign((1 << n), vector<int>(n, -1));
-
-    cout << "Enter the distance matrix:\n";
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            cin >> dist[i][j];
-
-    cout << "The minimum travelling cost is " << tsp(1, 0) << '\n';
-
-    return 0;
-}
 //sample input
 /*
 4
@@ -47,7 +9,7 @@ int main() {
 15 35 0 30
 20 25 30 0
 */
-/*
+
 Dikonda wala:
 #include <stdio.h>
 #include <limits.h>
@@ -95,4 +57,3 @@ int main() {
 
     return 0;
 }
-*/
