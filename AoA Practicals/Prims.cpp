68627ae45da5273be6b5194f33b0824020661793
Dhruv Harish Agrahari <122349013+Dhruvagrahari25@users.noreply.github.com>
2024-04-27 01:08:01 +0530

    Update Prims.cpp

--- AoA Practicals/Prims.cpp
+++ AoA Practicals/Prims.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef pair<int, int> P;
-//Function to find sum of weights of edges of the Minimum Spanning Tree.
+
 int spanningTree(int V, vector<vector<int>> adj[]) {
   priority_queue<P, vector<P>, greater<P>> pq;
   pq.push({0, 0});
@@ -9,54 +9,45 @@ int spanningTree(int V, vector<vector<int>> adj[]) {
   int sum = 0;
 
   while(!pq.empty()) {
+    auto p = pq.top();
+    pq.pop();
 
-      auto p = pq.top();
-      pq.pop();
+    int wt     = p.first;
+    int node   = p.second;
 
-      int wt     = p.first;
-      int node   = p.second;
+    if(inMST[node] == true) continue;
 
-      if(inMST[node] == true)
-          continue;
+    inMST[node] = true; //added to mst
+    sum += wt;
 
-      inMST[node] = true; //added to mst
-      sum += wt;
+    for(auto &tmp : adj[node]) {
 
-      for(auto &tmp : adj[node]) {
+      int neighbor      = tmp[0];
+      int neighbor_wt   = tmp[1];
 
-          int neighbor      = tmp[0];
-          int neighbor_wt   = tmp[1];
-
-          if(inMST[neighbor] == false) {
-              pq.push({neighbor_wt, neighbor});
-          }
-
-      }
+      if(inMST[neighbor] == false) pq.push({neighbor_wt, neighbor});
+    }
   }
   return sum;
 }
+
 int main() {
-  int V = 5;
+  int V, E;
+  cout << "Enter the number of vertices: ";
+  cin >> V;
+
   vector<vector<int>> adj[V];
 
-  // Add edges to the graph
-  adj[0].push_back({1, 2});
-  adj[1].push_back({0, 2});
+  cout << "Enter the number of edges: ";
+  cin >> E;
 
-  adj[1].push_back({2, 3});
-  adj[2].push_back({1, 3});
-
-  adj[0].push_back({3, 6});
-  adj[3].push_back({0, 6});
-
-  adj[1].push_back({3, 8});
-  adj[3].push_back({1, 8});
-
-  adj[1].push_back({4, 5});
-  adj[4].push_back({1, 5});
-
-  adj[2].push_back({4, 7});
-  adj[4].push_back({2, 7});
+  cout << "Enter the edges (source destination weight): \n";
+  for (int i = 0; i < E; ++i) {
+    int src, dest, wt;
+    cin >> src >> dest >> wt;
+    adj[src].push_back({dest, wt});
+    adj[dest].push_back({src, wt}); // Assuming undirected graph
+  }
 
   // Call the spanningTree function
   int weight = spanningTree(V, adj);
