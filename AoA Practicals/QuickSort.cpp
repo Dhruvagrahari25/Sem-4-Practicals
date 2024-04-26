f730f3d4cd825003885ff9631e87f91deb316b53
Dhruv Harish Agrahari <122349013+Dhruvagrahari25@users.noreply.github.com>
2024-04-27 00:09:04 +0530

    Update QuickSort.cpp

--- AoA Practicals/QuickSort.cpp
+++ AoA Practicals/QuickSort.cpp
@@ -3,24 +3,29 @@
 #include<algorithm>
 
 using namespace std;
+int partition(vector<int>& array, int low, int high){
+  int pivot=low;
+  int left = low;
+  int right = high;
+
+  while (left<right)
+  {
+    while (array[left]<=array[pivot] && left<high) left++;
+    while (array[right]>array[pivot] && right>low) right--;
+    if(left < right) swap(array[left],array[right]);
+  }
+  swap(array[pivot],array[right]);
+  return right;
+}
 void QuickSort(vector<int>& array, int low, int high){
-    if(high<=low) return;
+    if(high < low) return;
 
-    int pivot = array[high/2];
+    int pivot = (low+(high-1)/2);
 
-    int left = low;
-    int right = high;
+    int partitionIdx = partition(array,low,high);
 
-    while (left<right)
-    {
-        while (array[left]<=pivot && left<right) left++;
-        while (array[right]>=pivot && left<right) right--;
-        swap(array[left],array[right]);
-    }
-    swap(array[left],array[high]);
-
-    QuickSort(array,low,left-1);
-    QuickSort(array,left+1,high);
+    QuickSort(array,low,partitionIdx-1);
+    QuickSort(array,partitionIdx+1,high);
 }
 int main(){
     system("cls");
@@ -32,9 +37,9 @@ int main(){
     cout<<"Enter "<<n<<" Elements : ";
 
     for(int i=0; i<n; i++){
-        int temp;
-        cin>>temp;
-        array.push_back(temp);
+      int temp;
+      cin>>temp;
+      array.push_back(temp);
     }
 
     QuickSort(array,0,n-1);
