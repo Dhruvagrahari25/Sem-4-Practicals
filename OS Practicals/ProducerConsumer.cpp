c302a1bc2b21fc2096cf0118a915883a3c0cb7f4
Dhruv Harish Agrahari <122349013+Dhruvagrahari25@users.noreply.github.com>
2024-05-05 20:50:00 +0530

    Update ProducerConsumer.cpp

--- OS Practicals/ProducerConsumer.cpp
+++ OS Practicals/ProducerConsumer.cpp
@@ -1,63 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
-int full = 0;
-int empty = 10;
-int x = 0;
-int mutex = 1;
-void producer()
-{
-    mutex--;
-    full++;
-    empty--;
-    x++;
-    printf("PRODUCER PRODUCES THE ITEM %d \n", x);
-    mutex++;
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int bufferSize(0), curr(0);
+bool mutex = false;
+
+void produce(){
+  if(!mutex){
+    mutex=true;
+    if(curr<bufferSize){
+      curr++;
+      std::cout << "Producer produced item"<< curr << '\n';
+    }else std::cout << "Buffer Full!" << '\n';
+  }
+  mutex=false;
 }
-void consumer()
-{
-    mutex--;
-    full--;
-    empty++;
-    printf("CONSUMER CONSUMES THE ITEM %d \n", x);
-    x--;
-    mutex++;
+void consume(){
+  if(!mutex){
+    mutex=true;
+    if(curr>0){
+      curr--;
+      std::cout << "Consumer consumed item"<< curr << '\n';
+    }else std::cout << "Buffer Empty!" << '\n';
+  }
+  mutex=false;
 }
-int main()
-{
-    int n;
-    while (1)
-    {
-        printf("1. PRESS \"1\" FOR PRODUCER\n");
-        printf("2. PRESS \"2\" FOR CONSUMER\n");
-        printf("3. PRESS \"3\" FOR EXIT\n");
-        printf("ENTER THE CHOICE: ");
-        scanf("%d", &n);
-        switch (n)
-        {
-        case 1:
-            if (mutex == 1 && empty != 0)
-            {
-                producer();
-            }
-            else
-            {
-                printf("BUFFER IS FULL\n");
-            }
-            break;
-        case 2:
-            if (mutex == 1 && full != 0)
-            {
-                consumer();
-            }
-            else
-            {
-                printf("BUFFER IS EMPTY\n");
-            }
-            break;
-        case 3:
-            printf("\nAditya Dikonda\nS12\n24\n");
-            exit(0);
-        }
+int main(){
+  cout<<"Enter buffer Size: ";
+  cin>>bufferSize;
+
+  while(1){
+    std::cout << "\n\n" << '\n';
+    std::cout << "1. Produce" << '\n';
+    std::cout << "2. Consume" << '\n';
+    std::cout << "3. Exit" << '\n';
+    int choice(0);
+    cin>>choice;
+    switch (choice) {
+      case 1: produce();
+        break;
+
+      case 2: consume();
+        break;
+
+      case 3:
+        exit(1);
+
+      default: std::cout << "Invalid Choice!" << '\n';
     }
-    return 0;
+  }
+
+  return 0;
 }
